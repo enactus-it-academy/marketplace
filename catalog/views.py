9c328975b55893bb9dc12c18ce9bda8c44306bb0
0muraliev <omuraliev2019@gmail.com>
2021-03-20 13:53:07 +0600

    Add Product serializer and remove urls.py to user_account

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from catalog.models import Product
+from catalog.serializers import ProductSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
--- marketplace/urls.py
+++ marketplace/urls.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
 
+from catalog import views as catalog
+from user_account import views as user_account
+
+router = routers.DefaultRouter()
+
+router.register(r'users', user_account.UserViewSet)
+router.register(r'suppliers', user_account.SupplierViewSet)
+router.register(r'products', catalog.ProductViewSet)
 
 urlpatterns = [
-    path('', include('user_account.urls')),
+    path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
